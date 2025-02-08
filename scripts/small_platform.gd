26c53369179faa50f102bdb7c77cce3b43b5ba34
Ben Dowen <ben@dowen.me.uk>
2025-02-08 12:43:51 +0000

    extend level with cassie

--- scripts/small_platform.gd
+++ scripts/small_platform.gd
@@ -1,21 +1,21 @@
-extends Node2D
+extends AnimatableBody2D
 
 const SPEED = 30
 
 var direction = 1
 
+@onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if ray_cast_right.is_colliding():
 		direction = -1
-		animated_sprite.flip_h = true
+		sprite_2d.flip_h = true
 
 	if ray_cast_left.is_colliding():
 		direction = 1
-		animated_sprite.flip_h = false
+		sprite_2d.flip_h = false
 
 	position.x += direction * SPEED * delta
