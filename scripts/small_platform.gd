70a5f46b1105859b67caca9118645098ec054f94
Ben Dowen <ben@dowen.me.uk>
2025-02-16 21:22:05 +0000

    fix stutter small platform

--- scripts/small_platform.gd
+++ scripts/small_platform.gd
@@ -11,7 +11,7 @@ var direction = 1
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if ray_cast_right.is_colliding():
 		direction = -1
 
